3b85ce46a63fa81886a984509cb6bb36b530cf44
vahagntsarukyan <vahagnt@superannotate.com>
2025-05-01 22:50:39 +0400

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/solar/' : '/',   // ①
+  base: '/solar/',   // ①
   plugins: [react()]
 }))
